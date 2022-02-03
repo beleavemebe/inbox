f2d37c1174beaa0c9c040e3dfe21ceb257ca0932
Roman <cborzyy@gmail.com>
2022-02-03 17:02:49 +0300

    Do nothing on bottom nav item reselected

--- app/src/main/java/io/github/beleavemebe/inbox/ui/MainActivity.kt
+++ app/src/main/java/io/github/beleavemebe/inbox/ui/MainActivity.kt
@@ -56,6 +56,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initBottomNavigation() {
+        binding.mainBottomNavigationView.setOnItemReselectedListener {}
         NavigationUI.setupWithNavController(
             binding.mainBottomNavigationView,
             navController
