f2a31c15885eb95fc6ef63f876d78b7b2a7d3d2c
Roman <cborzyy@gmail.com>
2022-07-07 17:49:56 +0300

    Delete "information" refs & Replace "meetings" with "schedule"

--- app/src/main/java/io/github/beleavemebe/inbox/ui/infolist/InfoListViewModel.kt
+++
@@ -1,7 +0,0 @@
-package io.github.beleavemebe.inbox.ui.infolist
-
-import androidx.lifecycle.ViewModel
-
-class InfoListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
---
+++ app/src/main/java/io/github/beleavemebe/inbox/ui/schedule/ScheduleViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.beleavemebe.inbox.ui.schedule
+
+import androidx.lifecycle.ViewModel
+
+class ScheduleViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
