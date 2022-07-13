2d6a761e48f6da538129a05e8f658dbac5a57826
Roman <cborzyy@gmail.com>
2022-07-13 15:17:28 +0300

    Fix supernatural toolbar issue

--- app/src/main/java/io/github/beleavemebe/inbox/ui/MainActivity.kt
+++ app/src/main/java/io/github/beleavemebe/inbox/ui/MainActivity.kt
@@ -16,10 +16,11 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity(), ToolbarHost, NavigatorHost {
     private val binding by viewBinding(ActivityMainBinding::bind)
 
-    override val toolbar get() = binding.mainToolbar
-
     @Inject lateinit var navController: NavController
-    @Inject override lateinit var navigator: CrossFeatureNavigator
+    @Inject lateinit var _navigator: CrossFeatureNavigator
+
+    override val toolbar get() = binding.mainToolbar
+    override val navigator get() = _navigator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +35,9 @@ class MainActivity : AppCompatActivity(), ToolbarHost, NavigatorHost {
     }
 
     private fun initToolbar() {
+        binding.mainToolbar.setupWithNavController(navController, binding.root)
         setSupportActionBar(binding.mainToolbar)
+        // Drawer button is disabled, gotta call setup again xd
         binding.mainToolbar.setupWithNavController(navController, binding.root)
     }
 
